d786de1d8e3d5390837f9999610cb83b63a0056a
Jorge Mercado <jorgemercado212@gmail.com>
2021-04-08 11:47:44 -0400

    Creando nuevas rutas

--- hm/run.py
+++ hm/run.py
@@ -3,5 +3,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return ("Bienvenidos esta es la practica 1 de Jorge Alejandro mercado Araya")
+@app.route("/saludo")
+def saludo():
+    return ("Realizando nuevas rutas")   
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
